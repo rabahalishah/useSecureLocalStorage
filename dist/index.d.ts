20444ee697fbf138c94566257374ff5e59192504
Rabah Ali Shah <117630286+rabahalishah@users.noreply.github.com>
2024-11-24 18:36:20 +0500

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-type SetValue<T> = T | ((val: T) => T);
-declare function useSecureLocalStorage<T>(key: string, initialValue: T): [T, (value: SetValue<T>) => void];
-
-export { useSecureLocalStorage as default };
